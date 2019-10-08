88aa919a683ce57e7c0c5650bf985eb1d0bfc668
nisha <56124043+nisha0324@users.noreply.github.com>
2019-10-08 08:31:34 +0530

    Create typecasting.c
    typecasting (one of topics of c)

---
+++ typecasting.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() 
+{
+    // to convert into to float
+   /*int a=3;
+    float b=51;
+     printf("the value of b is %f",(float) a); */
+
+// to see int and int always returns an int
+   /* int a=3;
+    float b=51/5;
+    printf("the value of b is %f",b); */
+
+    int a=3;
+    float b=(float)51/5;
+    printf("the value of b is %f",b);
+    return 0;
+}

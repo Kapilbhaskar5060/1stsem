9d5d579870be9f7d268490b77a4567a1893ff0cb
nisha <56124043+nisha0324@users.noreply.github.com>
2019-10-12 17:32:21 +0530

    arrays in c

---
+++ arrays.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int marks[4];
+   
+   for (int i = 0; i < 4; i++)
+   {
+       printf("enter the value of %d element of the array\n",i);
+       scanf("%d",&marks[i]);
+   }
+
+   for (int i = 0; i < 4; i++)
+   {
+       printf("the value of %d element of the array is %d\n",i,marks[i]);
+
+   }
+  
+    return 0;
+}
